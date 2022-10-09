b87f9ce2e8e1531a2b6a6c7bd58d1ed4cd663312
Fangting Ma <64255604+Erica233@users.noreply.github.com>
2022-10-09 03:52:14 +0000

    script

--- operate.sh
+++ operate.sh
@@ -1,1 +1,11 @@
-echo "hi"
+#!/bin/bash
+lines=$(wc -l < $1)
+echo "The dataset" $1 "has" $lines "rows of data."
+
+LINE=1
+
+while read -r CURRENT_LINE
+	do
+		echo "$LINE: $CURRENT_LINE"
+    ((LINE++))
+done < $1
